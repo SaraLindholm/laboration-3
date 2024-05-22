20e79cfcb1d697cc9de5bdea93a7898804c3e44a
Sara Lindholm <sara.lindholm88@gmail.com>
2024-05-22 16:06:14 +0200

    Löst problem med formulär medlemskap samt lagt på en Toast

--- frontend/src/component/Membership.tsx
+++ frontend/src/component/Membership.tsx
@@ -1,63 +1,4 @@
-// import '../index.css'; // Din egen CSS-fil
-// import Button from 'react-bootstrap/Button';
-// import Form from 'react-bootstrap/Form';
-// import Col from 'react-bootstrap/Col';
-// import Row from 'react-bootstrap/Row';
-// import Container from 'react-bootstrap/Container';
-
-
-
-
-// function Membership() {
-
-//    const [formData, setFormData] = useState({
-//     surname: '',
-//     lastname: '',
-//     email: '',
-//     password: ''
-//   });
-
-
-
-//   return (
-//     <Container className="p-3">  {/* Container med padding */}
-//       <h1 className="mb-3">Bli medlem och hjälp kläderna cirkulera!</h1>
-//       <Form action="http://localhost:3000/membership" method="POST" onSubmit={e => e.preventDefault()}>
-//         <Row className="mb-1">
-//           <Form.Group as={Col} className="mb-1" controlId='formSurname'>
-//             <Form.Label column="lg">Förnamn </Form.Label>
-//             <Form.Control type="text" name="surname" placeholder="" />
-//           </Form.Group>
-
-//           <Form.Group  as={Col} className="mb-1" controlId="formLastname">
-//             <Form.Label column="lg">Efternamn </Form.Label>
-//             <Form.Control type="text" name="lastname" placeholder="" />
-//           </Form.Group>
-//           </Row>
-
-//           <Row className="mb-1">
-//             <Form.Group as={Col} className="mb-1" controlId="formEmail">
-//               <Form.Label column="lg">Email: </Form.Label>
-//               <Form.Control type="email" name="email" placeholder="" />
-//             </Form.Group>
-
-//             <Form.Group as={Col} className="mb-1" controlId="formPassword">
-//               <Form.Label column="lg">Lösenord: </Form.Label>
-//               <Form.Control type="password" name="password" placeholder="Ska bestå av minst 6 tecken" />
-//             </Form.Group>
-//             </Row>
-//               <Button variant="primary" type="submit">Bli medlem idag</Button>
-
-
-//       </Form>
-
-//     </Container>
-//   );
-// }
-
-// export default Membership;
-
-import { useState } from 'react';
+import React, { useState } from 'react';
 import '../index.css'; // Din egen CSS-fil
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
@@ -65,8 +6,9 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import { FormEvent } from 'react';
-
-// interface FormEvent extends React.FormEvent<HTMLFormElement> {}
+import { useNavigate } from 'react-router-dom'
+import Toast from 'react-bootstrap/Toast';
+import ToastContainer from 'react-bootstrap/ToastContainer';
 
 function Membership() {
   const [formData, setFormData] = useState({
@@ -76,8 +18,23 @@ function Membership() {
     password: ''
   });
 
+    // //ett försök till en Toast som informerar om medlemskap
+  const [showA, setShowA] = useState(false);
+  const toggleShowA = () => setShowA(!showA);
+  // const [position, setPosition] = useState('middle-center');
+
+  const navigate = useNavigate ()
+
+
+  const navigateToNewPage = () => {
+    console.log('Navigerar till ny sida vid klick')
+    navigate ('/rentClothes') }
+
+
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
+
+    console.log('Form data being sent:', formData);
 
     try {
       // Skicka formulärdata till servern
@@ -89,10 +46,18 @@ function Membership() {
         body: JSON.stringify(formData)
       });
 
+      console.log('Response status:', response.status);
+
       // Kontrollera om svaret är framgångsrikt
       if (response.ok) {
         // Visa ett meddelande eller utför andra åtgärder för en lyckad skickning
+
+        setShowA(true)
         console.log('Formuläret skickades framgångsrikt!');
+        setTimeout(() => {
+          navigateToNewPage ()
+        }, 5000)
+
       } else {
         // Visa ett meddelande eller utför andra åtgärder för en misslyckad skickning
         console.error('Något gick fel vid skickning av formuläret.');
@@ -111,6 +76,7 @@ function Membership() {
   };
 
   return (
+    <>
     <Container className="p-3">
       <h1 className="mb-3">Bli medlem och hjälp kläderna cirkulera!</h1>
       <Form onSubmit={handleSubmit}>
@@ -140,7 +106,21 @@ function Membership() {
 
         <Button variant="primary" type="submit">Bli medlem idag</Button>
       </Form>
+      <ToastContainer position="middle-center"  className="p-3" style={{ zIndex: 1 }}>
+      <Toast show={showA} onClose={toggleShowA}>
+        <Toast.Header>
+          <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+          <strong className="me-auto">Välkommen som medlem!</strong>
+          <small>Klädbiblioteket</small>
+        </Toast.Header>
+        <Toast.Body>Kul att du ville bli en av oss! Nu skickar vi dig vidare till alla fina kläder</Toast.Body>
+      </Toast>
+      </ToastContainer>
     </Container>
+
+   </>
+
+
   );
 }
 
