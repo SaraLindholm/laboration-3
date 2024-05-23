f69ce91d51d9ce81796bdbc22350e1e0dd290d1c
Sara Lindholm <sara.lindholm88@gmail.com>
2024-05-23 10:56:35 +0200

    samtliga formulär fungerar nu. Och Toast är tillagd

--- frontend/src/component/AddItemForm.tsx
+++ frontend/src/component/AddItemForm.tsx
@@ -4,10 +4,12 @@ import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
-// import {useEffect, useState} from 'react';
+import { FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom'
+import Toast from 'react-bootstrap/Toast';
+import ToastContainer from 'react-bootstrap/ToastContainer';
 
 function AddItemForm() {
-
 
   const categories: {category_id: number; name: string; } [] = [
     {category_id: 1, name: 'Tröjor' },
@@ -21,45 +23,111 @@ function AddItemForm() {
     {category_id: 9, name: 'T-shirts'},
     {category_id: 10, name: 'Övrigt'}
   ]
+
+  const [formData, setFormData] = useState ( {
+    name: '',
+    description: '',
+    brand: '',
+    size: '',
+    color: '',
+    condition_comment: '',
+    category_id: '',
+    image_url: ''
+
+  })
+
+      // //ett försök till en Toast som informerar om medlemskap
+      const [showA, setShowA] = useState(false);
+      const toggleShowA = () => setShowA(!showA);
+
+  const navigate = useNavigate ()
+
+
+  const navigateToNewPage = () => {
+    console.log('Navigerar till ny sida vid klick')
+    navigate ('/rentClothes') }
+
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log('Form data being sent:', formData);
+
+    try {
+      // Skicka formulärdata till servern
+      const response = await fetch('http://localhost:3000/upload', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+
+      console.log('Response status:', response.status);
+
+      if (response.ok) {
+        // Toast
+        setShowA(true)
+        console.log('Formuläret skickades framgångsrikt!');
+        setTimeout(() => {
+          navigateToNewPage ()
+        }, 5000)
+
+      } else {
+        console.error('Något gick fel vid skickning av formuläret.');
+      }
+    } catch (error) {
+      console.error('Ett fel uppstod:', error);
+    }
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
   return (
+  <>
     <Container className="p-3">  {/* Container med padding */}
 
       <h1 className="mb-4">Ladda upp kläder till vårt gemensamma bibliotek</h1>
-      <Form action="http://localhost:3000/upload" method="POST" encType="multipart/form-data"
-      >
+      <Form onSubmit={handleSubmit}
+            encType="multipart/form-data">
       <Row className="mb-3">
         <Form.Group as={Col} className="mb-3" controlId='formName'>
           <Form.Label column="lg">Titel: </Form.Label>
-          <Form.Control type="text" name="name" placeholder="T.ex 'Snygg vårblus'" />
+          <Form.Control type="text" name="name" placeholder="T.ex 'Snygg vårblus'" value={formData.name} onChange={handleChange}/>
         </Form.Group>
 
 
         <Form.Group  as={Col} className="mb-3" controlId="formDescription">
           <Form.Label column="lg">Bekriv din vara: </Form.Label>
-          <Form.Control type="text" name="description" placeholder="T.ex 'Bara använd ett par gånger, smickarande passform'" />
+          <Form.Control type="text" name="description" placeholder="T.ex 'Bara använd ett par gånger, smickarande passform'" value={formData.description} onChange={handleChange}/>
         </Form.Group>
         </Row>
 
         <Row className="mb-3">
           <Form.Group as={Col} className="mb-3" controlId="formBrand">
             <Form.Label column="lg">Märke: </Form.Label>
-            <Form.Control type="text" name="brand" placeholder="Märke" />
+            <Form.Control type="text" name="brand" placeholder="Märke" value={formData.brand} onChange={handleChange} />
           </Form.Group>
 
           <Form.Group as={Col} className="mb-3" controlId="formSize">
             <Form.Label column="lg">Storlek: </Form.Label>
-            <Form.Control type="text" name="size" placeholder="Storlek" />
+            <Form.Control type="text" name="size" placeholder="Storlek" value={formData.size} onChange={handleChange}/>
           </Form.Group>
 
           <Form.Group as={Col} className="mb-3" controlId="formColor">
             <Form.Label column="lg">Färg: </Form.Label>
-            <Form.Control type="text" name="color" placeholder="Färg" />
+            <Form.Control type="text" name="color" placeholder="Färg" value={formData.color} onChange={handleChange}/>
           </Form.Group>
         </Row>
 
         <Form.Group className="mb-3" controlId="formConditionComment">
           <Form.Label column="lg">Skick: </Form.Label>
-          <Form.Control type="text" name="condition_comment" placeholder="Skick" />
+          <Form.Control type="text" name="condition_comment" placeholder="Skick" value={formData.condition_comment} onChange={handleChange}/>
           <Form.Text className="text-muted">Ange om plagget har eventuella skador/slitage:</Form.Text>
         </Form.Group>
 
@@ -72,7 +140,9 @@ function AddItemForm() {
               name="category_id"
               id={`category-${category.category_id}`}
               value={category.category_id}
-              label={category.name}
+              label={category.name} onChange={handleChange}
+              // value={formData.category_id}
+              //jag vet inte hur jag ska använda mig av formData här? Jag har ju angivet value för respektive kategori här?
             />
         </div>
           ))}
@@ -86,17 +156,99 @@ function AddItemForm() {
 
         <Button variant="primary" type="submit">Ladda upp ditt plagg</Button>
       </Form>
-{/*
-      <Form action="http://localhost:3000/upload" method="POST" encType="multipart/form-data"
-      >
-        <Form.Group className="mb-3" controlId="formImageUrl">
-          <Form.Label column="lg">Ladda upp en bild: </Form.Label>
-          <Form.Control type="file" name="image_url" placeholder="Bild URL" />
-        </Form.Group>
 
-        <Button variant="primary" type="submit">Ladda upp ditt plagg</Button></Form> */}
+      <ToastContainer position="middle-center"  className="p-3" style={{ zIndex: 1 }}>
+      <Toast show={showA} onClose={toggleShowA}>
+        <Toast.Header>
+          <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+          <strong className="me-auto">Tack för att du delar med dig</strong>
+          <small>Klädbiblioteket</small>
+        </Toast.Header>
+        <Toast.Body>Ditt plagg är nu tillagt i biblioteket.</Toast.Body>
+      </Toast>
+      </ToastContainer>
+
     </Container>
+    </>
   );
 }
 
+
+
 export default AddItemForm;
+
+
+// //kopierar för att spara en fungerande return
+// return (
+//   <Container className="p-3">  {/* Container med padding */}
+
+//     <h1 className="mb-4">Ladda upp kläder till vårt gemensamma bibliotek</h1>
+//     <Form action="http://localhost:3000/upload" method="POST" encType="multipart/form-data"
+//     >
+//     <Row className="mb-3">
+//       <Form.Group as={Col} className="mb-3" controlId='formName'>
+//         <Form.Label column="lg">Titel: </Form.Label>
+//         <Form.Control type="text" name="name" placeholder="T.ex 'Snygg vårblus'" />
+//       </Form.Group>
+
+
+//       <Form.Group  as={Col} className="mb-3" controlId="formDescription">
+//         <Form.Label column="lg">Bekriv din vara: </Form.Label>
+//         <Form.Control type="text" name="description" placeholder="T.ex 'Bara använd ett par gånger, smickarande passform'" />
+//       </Form.Group>
+//       </Row>
+
+//       <Row className="mb-3">
+//         <Form.Group as={Col} className="mb-3" controlId="formBrand">
+//           <Form.Label column="lg">Märke: </Form.Label>
+//           <Form.Control type="text" name="brand" placeholder="Märke" />
+//         </Form.Group>
+
+//         <Form.Group as={Col} className="mb-3" controlId="formSize">
+//           <Form.Label column="lg">Storlek: </Form.Label>
+//           <Form.Control type="text" name="size" placeholder="Storlek" />
+//         </Form.Group>
+
+//         <Form.Group as={Col} className="mb-3" controlId="formColor">
+//           <Form.Label column="lg">Färg: </Form.Label>
+//           <Form.Control type="text" name="color" placeholder="Färg" />
+//         </Form.Group>
+//       </Row>
+
+//       <Form.Group className="mb-3" controlId="formConditionComment">
+//         <Form.Label column="lg">Skick: </Form.Label>
+//         <Form.Control type="text" name="condition_comment" placeholder="Skick" />
+//         <Form.Text className="text-muted">Ange om plagget har eventuella skador/slitage:</Form.Text>
+//       </Form.Group>
+
+//       <Form.Group className="mb-3" controlId="formCategoryId">
+//         <Form.Label column="lg">Kategori av plagg: </Form.Label>
+//         {categories.map((category) => (
+//           <div key={`category-${category.category_id}`} className="mb-3">
+//           <Form.Check
+//             type="radio"
+//             name="category_id"
+//             id={`category-${category.category_id}`}
+//             value={category.category_id}
+//             label={category.name}
+//           />
+//       </div>
+//         ))}
+
+//       </Form.Group>
+
+//        <Form.Group className="mb-3" controlId="formImageUrl">
+//         <Form.Label column="lg">Ladda upp en bild: </Form.Label>
+//         <Form.Control type="file" name="image_url" placeholder="Bild URL" />
+//       </Form.Group>
+
+//       <Button variant="primary" type="submit">Ladda upp ditt plagg</Button>
+//     </Form>
+
+//   </Container>
+// );
+// }
+
+
+
+// export default AddItemForm;
